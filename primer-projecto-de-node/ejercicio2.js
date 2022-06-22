20ac44176aa357f485d93b8b70e4b850cfd2face
latinogan <roger.castillo.arce@gmail.com>
2022-06-22 06:58:15 -0500

    second exercise js

---
+++ primer-projecto-de-node/ejercicio2.js
@@ -0,0 +1,15 @@
+let lista =[
+    "Roger",
+    26 ,
+    true ,
+    Date ()
+]
+let libro= {
+    titulo: "Cien años de soledad" ,
+    autor: "Gabriel García Márquez",
+    fecha: 1970 ,
+    url: "https://es.wikipedia.org/wiki/Cien_a%C3%B1os_de_soledad"
+
+}
+console.log(lista)
+console.log(libro)
---
+++ primer-projecto-de-node/iniciacion-tipos.js
@@ -0,0 +1,14 @@
+// tipos primitivos 
+
+//number 
+4;
+0;
+
+//string
+"hola mundo"
+
+//boolean
+true;
+false;
+
+// nulo and undefined
