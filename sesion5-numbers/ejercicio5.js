29241a07d2d64f7ad8b7664fc1268e5a58ae45d1
latinogan <roger.castillo.arce@gmail.com>
2022-06-29 05:40:16 -0500

    5 exercise js

--- sesion5-numbers/ejercicio5.js
+++ sesion5-numbers/ejercicio5.js
@@ -2,10 +2,10 @@ let altura_cm = 180;
 const altura_m = 1.80;
 const peso_kg = 90.0;
 
-const altura_red = Math.ceil(altura_m)
-console.log( altura_red)
+const altura_red = Math.ceil(altura_m);
+console.log( altura_red);
 
-const peso_red = Math.floor(peso_kg)
-console.log(peso_red)
+const peso_red = Math.floor(peso_kg);
+console.log(peso_red);
 
 const sonIguales = Number.MAX_VALUE + 1 === Number.MAX_VALUE
