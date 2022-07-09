5080d1e21ef09d065e89751e382d984a88179008
latinogan <roger.castillo.arce@gmail.com>
2022-07-09 17:28:46 -0500

    ejercisio 8 function

--- primer-projecto-de-node/08.funciones/ejercisio-function.js
+++ primer-projecto-de-node/08.funciones/ejercisio-function.js
@@ -1,11 +1,14 @@
+//Una función sin parámetros que devuelva siempre "true"
 function devuelveTrue() {
     return true
 }
 
-async function damePromesa() {
+//Una función asíncrona que utilice un setTimeout y pase por consola un "Hola soy una promesa" 5 segundos después de haberse ejecutado
+async function Promesa() {
     return setTimeout(() => console.log("Hola soy una promesa"), 5000)
 }
 
+//Una función generadora de índices pares automáticos
 function* idsPares() {
     let id = 0
     while(true) {
