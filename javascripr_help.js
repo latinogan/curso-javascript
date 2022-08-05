5f2d01fd1e562b2014adef49aa12ef87805fd1a5
roger smith <74432345+latinogan@users.noreply.github.com>
2022-08-05 17:55:01 -0500

    Update javascripr_help.js

--- javascripr_help.js
+++ javascripr_help.js
@@ -68,3 +68,16 @@ const foundAnimal= animals.findIndex(animals => {
   return animals == 'elephant'
 })
 //console.log(foundAnimal) result 7
+
+
+// .reduce 
+
+const newNumbers = [1, 3, 5, 7];
+
+const newSum=newNumbers.reduce((accumulator , currentValue) => {
+  console.log('The value of accumulator: ', accumulator);
+console.log('The value of currentValue: ', currentValue);
+return accumulator +currentValue
+} ,10)
+
+console.log(newSum) //26
